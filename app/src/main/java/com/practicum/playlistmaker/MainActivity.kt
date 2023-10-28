05513b98358a78734f09efd2780a94e6b532307a
Damir-jan <damgaripov1@yandex.ru>
2023-10-28 18:14:53 +0500

    мой третий коммит

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -11,20 +11,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val SearchButton = findViewById<Button>(R.id.Search)
-        val MediatekaButton = findViewById<Button>(R.id.Mediateka)
-        val SettingsButton = findViewById<Button>(R.id.Settings)
+        val searchButton = findViewById<Button>(R.id.Search)
+        val mediatekaButton = findViewById<Button>(R.id.Mediateka)
+        val settingsButton = findViewById<Button>(R.id.Settings)
 
 
-        SettingsButton.setOnClickListener {
+        settingsButton.setOnClickListener {
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
         }
-        MediatekaButton.setOnClickListener {
+        mediatekaButton.setOnClickListener {
             val displayIntent = Intent(this, MediatekaActivity::class.java)
             startActivity(displayIntent)
         }
-        SearchButton.setOnClickListener {
+        searchButton.setOnClickListener {
             val displayIntent = Intent(this, SearchActivity::class.java)
             startActivity(displayIntent)
         }
