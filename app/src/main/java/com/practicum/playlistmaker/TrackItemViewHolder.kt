7e1a69c34761c47c4af0a9ebe0177aabdfdad7f8
Damir-jan <damgaripov1@yandex.ru>
2023-12-17 17:35:48 +0500

    мой второй коммит 10 спринт

--- app/src/main/java/com/practicum/playlistmaker/TrackItemViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackItemViewHolder.kt
@@ -1,9 +1,8 @@
 package com.practicum.playlistmaker
 
 import Track
-import android.graphics.drawable.Drawable
-import android.text.SpannableString
-import android.text.style.ImageSpan
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -16,7 +15,6 @@ class TrackItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val textViewFirst: TextView = itemView.findViewById(R.id.textViewFirst)
     private val textViewSecond: TextView = itemView.findViewById(R.id.textViewSecond)
-    private val imageViewVector: ImageView = itemView.findViewById(R.id.imageViewVector)
         private val as_TrackName: TextView = itemView.findViewById(R.id.as_TrackName)
         private val imageView: ImageView = itemView.findViewById(R.id.image_album)
 
@@ -26,11 +24,19 @@ class TrackItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         textViewFirst.text = model.artistName
         textViewSecond.text = model.trackTime
 
+
         Glide.with(itemView.context)
             .load(model.artworkUrl100)
-            .transform(RoundedCorners(2))
+            .transform(RoundedCorners(dpToPx(2, itemView.context)))
             .fitCenter()
             .placeholder(R.drawable.placeholder)
             .into(imageView)
+    }
+    fun dpToPx(dp: Int, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            context.resources.displayMetrics
+        ).toInt()
     }
 }
